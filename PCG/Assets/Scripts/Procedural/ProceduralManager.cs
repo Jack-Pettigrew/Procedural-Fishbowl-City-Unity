a14fc0d5fc596cd840c39eeb85b6cfa0a0bc549c
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-04-25 14:23:35 +0100

    Added Fish Person + Started NPC Spawner

--- PCG/Assets/Scripts/Procedural/ProceduralManager.cs
+++ PCG/Assets/Scripts/Procedural/ProceduralManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 // Procedural Manager Class - determines Procedural Content Generation
 public class ProceduralManager : MonoBehaviour
 {
-    [Header("Parent"), Tooltip("The 'Folder' gameobject to store all 'Building Folders'.")]
+    [Header("Parents"), Tooltip("The 'Folder' gameobject to store all 'Building Folders'.")]
     public Transform buildingParent;
 
     [Header("Road PCG")]
@@ -16,33 +16,17 @@ public class ProceduralManager : MonoBehaviour
     public int buildingsMaxMid = 1;
     public List<GameObject> buildingBases, buildingMids, buildingRoofs;
 
+    [Header("NPCs")]
+    public List<GameObject> npcList;
+
     [HideInInspector]
     public float[] randomYRotation = { 0, 90, 180, 270 };
     private static int buildingNumber = 0;
 
     [Header("Terrain PCG")]
-    public Terrain cityTerrain;
-    public float totalTerrainHeight, totalTerrainLength;
-    public List<Terrain> terrainList;
+    public float terrainWidth;
+    public float terrainLength;
 
-    public  
-
-    // Set Handles
-    void Start()
-    {
-        // Get Terrain Information
-        terrainList.AddRange(FindObjectsOfType<Terrain>());
-
-        for (int i = 0; i < terrainList.Count; i++)
-        {
-            totalTerrainHeight += terrainList[i].terrainData.size.x;
-            totalTerrainLength += terrainList[i].terrainData.size.z;
-
-            if (terrainList[i].tag == "City_Terrain")
-                cityTerrain = terrainList[i];
-        }
-
-    }
 
     public void BakeRoadNavMesh()
     {
