116622cef6b4a42f961bdf5e167f747d7e725a53
Jack-Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-04-29 15:55:33 +0100

    Started Building Overlap Script

---
+++ PCG/Assets/Scripts/BuildingOverlapHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingOverlapHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Bank" || col.name == "Library")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
