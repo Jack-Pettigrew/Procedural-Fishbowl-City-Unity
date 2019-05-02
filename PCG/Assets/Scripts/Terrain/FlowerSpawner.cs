0013879358d44bcbd4e257088540cb14bd397060
Jack Pettigrew <36480371+Jack-Pettigrew@users.noreply.github.com>
2019-05-02 03:09:15 +0100

    Added Random Flower Spawner on Trees + Big Fish Man and Peeking Animation

---
+++ PCG/Assets/Scripts/Terrain/FlowerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] flowers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(flowers[Random.Range(0, flowers.Length)], GetRandomPos(), Quaternion.identity).transform.SetParent(transform);
+        }
+    }
+
+    private Vector3 GetRandomPos()
+    {
+        Vector3 pos;
+
+        float distance = 6.0f;
+
+        pos = new Vector3(Random.Range(-distance, distance), 0, Random.Range(-distance, distance));
+
+        pos = transform.position + pos;
+
+        return pos;
+    }
+}
